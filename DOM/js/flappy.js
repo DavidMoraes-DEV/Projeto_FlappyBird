44f5a1c55224720a59b9654c396c331c624ffe15
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-11-07 14:28:06 -0300

    Projeto: Flappy Bird parte 02

--- DOM/js/flappy.js
+++ DOM/js/flappy.js
@@ -1,1 +1,50 @@
-// ...
+function novoElemento(tagName, className) {
+    const elem = document.createElement(tagName)
+    elem.className = className
+    return elem
+}
+
+/* Definicoes das barreiras do jogo: */
+function Barreira(reversa = false) {
+    this.elemento = novoElemento('div', 'barreira')
+
+    const borda = novoElemento('div', 'borda')
+    const corpo = novoElemento('div', 'corpo')
+    this.elemento.appendChild(reversa ? corpo : borda)
+    this.elemento.appendChild(reversa ? borda : corpo)
+
+    this.setAltura = altura => corpo.style.height = `${altura}px`
+}
+
+/*const b = new Barreira(true)
+b.setAltura(200)
+document.querySelector('[dm-flappy]').appendChild(b.elemento)*/
+
+/* Definicao das posicoes do par de barreiras de forma aleatoria */
+function ParDeBarreira(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+}
+
+/* Teste de renderizacao das barreiras */
+const b = new ParDeBarreira(700, 300, 400)
+document.querySelector('[dm-flappy]').appendChild(b.elemento)
